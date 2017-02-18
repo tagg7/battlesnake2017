9fa7e15c286b2215f99186b715910fd305d15a31
Stephen Bates <me@stephenbates.ca>
2017-02-18 21:47:41 +0000

    Setting up workspace; dumb snake

---
+++ web.rb
@@ -0,0 +1,50 @@
+require 'sinatra'
+require 'json'
+
+get '/' do
+    responseObject = {
+        "color"=> "#fff000",
+        "head_url"=> "url/to/your/img/file"
+    }
+
+    return responseObject.to_json
+end
+
+post '/start' do
+    requestBody = request.body.read
+    requestJson = requestBody ? JSON.parse(requestBody) : {}
+
+    # Get ready to start a game with the request data
+
+    # Dummy response
+    responseObject = {
+        "taunt" => "battlesnake-ruby",
+    }
+
+    return responseObject.to_json
+end
+
+post '/move' do
+    requestBody = request.body.read
+    requestJson = requestBody ? JSON.parse(requestBody) : {}
+
+    # Calculate a move with the request data
+
+    # Dummy response
+    responseObject = {
+        "move" => "north", # One of either "north", "east", "south", or "west".
+        "taunt" => "going north!",
+    }
+
+    return responseObject.to_json
+end
+
+post '/end' do
+    requestBody = request.body.read
+    requestJson = requestBody ? JSON.parse(requestBody) : {}
+
+    # No response required
+    responseObject = {}
+
+    return responseObject.to_json
+end
