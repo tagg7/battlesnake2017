cd43af5791b180f01ca02419913a564244c83c7b
Stephen Bates <me@stephenbates.ca>
2017-03-01 03:40:09 +0000

    Cleaning up the code to use more methods, added comments, corrected some logic

--- web.rb
+++ web.rb
@@ -52,6 +52,7 @@ post '/move' do
     downMoveScore = calculateScoreForSnakeMovement(board, snakesLookup, foods, snakeId, 0, 1, boardWidth, boardHeight)
     puts "Down Score = #{downMoveScore}"
     
+    # Select the move with the highest score
     if leftMoveScore >= rightMoveScore and leftMoveScore >= upMoveScore and leftMoveScore >= downMoveScore
         move = "left"
     end
@@ -65,7 +66,7 @@ post '/move' do
         move = "down"
     end
 
-    # Response
+    # Set the response
     responseObject = {
         "move" => move,
         "taunt" => "Gotta go fast!",
@@ -74,72 +75,85 @@ post '/move' do
     return responseObject.to_json
 end
 
+=begin
+This method calculates a score that should be used to determine which direction to move the snake for the given x- and y-offset.
+A score of -1 means that the direction is a wall or snake.
+=end
 def calculateScoreForSnakeMovement(board, snakes, foods, snakeId, xOffset, yOffset, boardWidth, boardHeight)
     snake = snakes[snakeId]
-    currentHeadCoord = snake.coords[0]
+    snakeHead = snake.coords[0]
     
-    # Verify that this position is not outside the board area
-    if currentHeadCoord[0] + xOffset < 0 or currentHeadCoord[0] + xOffset >= boardWidth
+    xPosition = snakeHead[0] + xOffset
+    yPosition = snakeHead[1] + yOffset
+    
+    # Verify that the square does not contain another snake or is a wall
+    validMove = snakeCanMoveToPosition(xPosition, yPosition, board, snakes, boardWidth, boardHeight)
+    if validMove == false
         return -1
     end
-    if currentHeadCoord[1] + yOffset < 0 or currentHeadCoord[1] + yOffset >= boardHeight
-        return -1
-    end
     
-    # Verify that this position is not another snake's body
-    currentObjectInPosition = board[[currentHeadCoord[0] + xOffset, currentHeadCoord[1] + yOffset]]
-    if currentObjectInPosition.is_a?(SnakeSegment)
-        matchingSnake = snakes[currentObjectInPosition.id]
-        puts matchingSnake.coords[matchingSnake.coords.length-1]
-        
-        # If this position is currently a snake's tail, then we can safely access it because it will be moved further by next turn
-        if matchingSnake.coords[matchingSnake.coords.length-1] != [currentHeadCoord[0] + xOffset, currentHeadCoord[1] + yOffset]
-            return -1
-        end
-    end
-    
-    # Modify snake to new coordinates
-    snakesClone = Marshal.load(Marshal.dump(snakes))
-    snakeClone = Marshal.load(Marshal.dump(snake))
-    
-    for i in 1..(snakeClone.coords.length - 1)
-        snakeClone.coords[i] = snakeClone.coords[i-1]
-    end
-    snakeClone.coords[0] = [currentHeadCoord[0] + xOffset, currentHeadCoord[1] + yOffset]
-    snakesClone[snakeId] = snakeClone
-    
-    # # Modify board to move snake to given position
-    # boardClone = board.clone
-    
-    # boardClone[[currentHeadCoord[0] + xOffset, currentHeadCoord[1] + yOffset]] = SnakeSegment.new(snakeId, 0, snake.totalLength, snake.health)
-    # for i in 0..(snake.totalLength - 2)
-    #     boardClone[snake.coords[i]].segmentNum += 1
-    # end
-    # if snake.coords[snake.totalLength-2] != snake.coords[snake.totalLength-1]
-    #     boardClone[snake.coords[snake.totalLength-1]] = BLANK_VALUE
-    # end
+    # Modify the snakes lookup to simulate the snake moving to the specified position
+    snakesClone = moveSnakeInSnakesLookup(snakes, snakeId, xPosition, yPosition)
+    #boardClone = moveSnakeInBoard(board, snakeId, xPosition, yPosition)
     
     # Calculate score for each source of food
     foodScore = 0
-    for i in 0..(foods.length-1)
+    for i in 0..(foods.length - 1)
         foodScore += calculateScoreForSnakesProximityToFood(snakesClone, snakeId, foods[i])
     end
     
-    # Weight the food score based on the snake's health
+    # Weight the food score based on the snake's remaining health
     foodScore *= 100 - snake.health
     
     # TODO: Verify that snake can get back to its own tail
     
     # TODO: Generate score based on the number of squares that the snake can access from this location
     
-    return foodScore
+    totalScore = foodScore
+    
+    return totalScore
 end
 
+=begin
+This method returns true if the specified position is empty (and inside the board). If the space is currently occupied by a snake's tail, then it will also return true.
+For all other scenarios, returns false.
+=end
+def snakeCanMoveToPosition(xPosition, yPosition, board, snakes, boardWidth, boardHeight)
+    # Verify that this position is not outside the board area
+    if xPosition < 0 or xPosition >= boardWidth
+        return false
+    end
+    if yPosition < 0 or yPosition >= boardHeight
+        return false
+    end
+    
+    # Verify that this position is not another snake's body
+    currentObjectInPosition = board[[xPosition, yPosition]]
+    if currentObjectInPosition.is_a?(SnakeSegment)
+        matchingSnake = snakes[currentObjectInPosition.id]
+        
+        # If this position is currently a snake's tail, then we can safely access it because it will be moved further by next turn
+        # NOTE: Edge case when starting out - body is collapsed onto multiple squares
+        if (matchingSnake.coords[matchingSnake.coords.length-1] == [xPosition, yPosition]) and (matchingSnake.coords[matchingSnake.coords.length-1] != matchingSnake.coords[matchingSnake.coords.length-2])
+            return true
+        end
+        
+        return false
+    end
+    
+    return true
+end
+
+=begin
+This method returns a score out of 100 based on how close the food is. If another snake is closer to the food than the selected snake, then the score is 0.
+=end
 def calculateScoreForSnakesProximityToFood(snakes, snakeId, food)
     snakeHead = snakes[snakeId].coords[0]
     
+    # Calculate our distance to the food
     distance = distanceToFood(snakeHead, food)
-    puts "Distance to food = #{distance}"
+    
+    # Determine whether another snake is closer to the food than us
     snakes.each do |otherSnakeId, otherSnake|
         if otherSnakeId == snakeId
             next
@@ -154,6 +168,30 @@ def calculateScoreForSnakesProximityToFood(snakes, snakeId, food)
     return 100 - distance
 end
 
+=begin
+This method returns the exact distance to the selected food in squares.
+=end
+def distanceToFood(snakeHead, food)
+    snakeHeadX = snakeHead[0];
+    snakeHeadY = snakeHead[1];
+    
+    foodX = food[0];
+    foodY = food[1];
+    
+    if snakeHeadX == foodX and snakeHeadY == foodY
+        return 0
+    end
+    
+    return (foodX - snakeHeadX).abs + (foodY - snakeHeadY).abs
+end
+
+=begin
+This method generates a two dimensional board.
+Each square with a snake in it contains a SnakeSegment object (containing the snake ID, segment number, length and health of the snake it is a part of).
+Each square with food in it contains a food value (1).
+Each blank square contains a blank value (0).
+Note that the hash table is not bound by the height and width, so that must be calculated separately.
+=end
 def generateBoard(snakes, foods)
     board = Hash.new(BLANK_VALUE);    # Using a hash because inserts are O(1), and it sets default values for all coordinates
     
@@ -183,6 +221,9 @@ def generateBoard(snakes, foods)
     return board
 end
 
+=begin
+This method generates a lookup dictionary for snakes, with the key being the snake ID and the value containing information about the snake.
+=end
 def generateSnakesLookup(snakes)
     snakesLookup = Hash.new
     
@@ -199,66 +240,43 @@ def generateSnakesLookup(snakes)
     return snakesLookup
 end
 
-def canMoveLeft(board, snakeHead)
-    snakeHeadX = snakeHead[0];
-    snakeHeadY = snakeHead[1];
+=begin
+Returns a cloned snakes lookup object, with the selected snake moved to the given coordinates
+=end
+def moveSnakeInSnakesLookup(snakes, snakeId, headXPosition, headYPosition)
+    snake = snakes[snakeId]
     
-    leftX = snakeHeadX - 1
-    if leftX >= 0 and (board[[leftX, snakeHeadY]] == BLANK_VALUE or board[[leftX, snakeHeadY]] == FOOD_VALUE)
-        return true
+    # Perform a deep clone
+    snakesClone = Marshal.load(Marshal.dump(snakes))
+    snakeClone = Marshal.load(Marshal.dump(snake))
+    
+    for i in 1..(snakeClone.coords.length - 1)
+        snakeClone.coords[i] = snakeClone.coords[i-1]
     end
+    snakeClone.coords[0] = [headXPosition, headYPosition]
+    snakesClone[snakeId] = snakeClone
     
-    return false
+    return snakesClone
 end
 
-def canMoveRight(board, snakeHead, boardWidth)
-    snakeHeadX = snakeHead[0];
-    snakeHeadY = snakeHead[1];
+=begin
+Returns a clone board object, with the selected snake moved to the given coordinates
+=end
+def moveSnakeInBoard(board, snakeId, headXPosition, headYPosition)
+    snake = snakes[snakeId]
     
-    rightX = snakeHeadX + 1
-    if rightX < boardWidth and (board[[rightX, snakeHeadY]] == BLANK_VALUE or board[[rightX, snakeHeadY]] == FOOD_VALUE)
-        return true
+    # Perform a deep clone
+    boardClone = Marshal.load(Marshal.dump(board))
+    
+    boardClone[[headXPosition, headYPosition]] = SnakeSegment.new(snakeId, 0, snake.totalLength, snake.health)
+    for i in 0..(snake.totalLength - 2)
+        boardClone[snake.coords[i]].segmentNum += 1
+    end
+    if snake.coords[snake.totalLength - 2] != snake.coords[snake.totalLength - 1]
+        boardClone[snake.coords[snake.totalLength-1]] = BLANK_VALUE
     end
     
-    return false
-end
-
-def canMoveDown(board, snakeHead, boardHeight)
-    snakeHeadX = snakeHead[0];
-    snakeHeadY = snakeHead[1];
-    
-    downY = snakeHeadY + 1
-    if downY < boardHeight and (board[[snakeHeadX, downY]] == BLANK_VALUE or board[[snakeHeadX, downY]] == FOOD_VALUE)
-        return true
-    end
-    
-    return false
-end
-
-def canMoveUp(board, snakeHead)
-    snakeHeadX = snakeHead[0];
-    snakeHeadY = snakeHead[1];
-    
-    upY = snakeHeadY - 1
-    if upY >= 0 and (board[[snakeHeadX, upY]] == BLANK_VALUE or board[[snakeHeadX, upY]] == FOOD_VALUE)
-        return true
-    end
-    
-    return false
-end
-
-def distanceToFood(snakeHead, food)
-    snakeHeadX = snakeHead[0];
-    snakeHeadY = snakeHead[1];
-    
-    foodX = food[0];
-    foodY = food[1];
-    
-    if snakeHeadX == foodX and snakeHeadY == foodY
-        return 0
-    end
-    
-    return (foodX - snakeHeadX).abs + (foodY - snakeHeadY).abs
+    return boardClone
 end
 
 class SnakeSegment
